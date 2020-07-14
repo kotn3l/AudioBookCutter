acdcf73704e59a93475f42a660bc799f208249b7
Kornel Mirkovszki <kotn3l@gmail.com>
2020-07-14 12:19:07 +0200

    Kezdetleges menü és hang megjelenítés

--- AudioBookCutter/AudioBookCutter/MainWindow.Designer.cs
+++ AudioBookCutter/AudioBookCutter/MainWindow.Designer.cs
@@ -28,20 +28,87 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.mainMenu1 = new System.Windows.Forms.MainMenu(this.components);
+            this.menuItem1 = new System.Windows.Forms.MenuItem();
+            this.openAudio = new System.Windows.Forms.MenuItem();
+            this.openMarker = new System.Windows.Forms.MenuItem();
+            this.saveMarker = new System.Windows.Forms.MenuItem();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 31);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(775, 264);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // mainMenu1
+            // 
+            this.mainMenu1.MenuItems.AddRange(new System.Windows.Forms.MenuItem[] {
+            this.menuItem1});
+            // 
+            // menuItem1
+            // 
+            this.menuItem1.Index = 0;
+            this.menuItem1.MenuItems.AddRange(new System.Windows.Forms.MenuItem[] {
+            this.openAudio,
+            this.openMarker,
+            this.saveMarker});
+            this.menuItem1.Text = "Fájl";
+            // 
+            // openAudio
+            // 
+            this.openAudio.Index = 0;
+            this.openAudio.Text = "Hanganyag(ok) megnyitása...";
+            this.openAudio.Click += new System.EventHandler(this.openAudio_Click);
+            // 
+            // openMarker
+            // 
+            this.openMarker.Enabled = false;
+            this.openMarker.Index = 1;
+            this.openMarker.Text = "Marker megnyitása...";
+            // 
+            // saveMarker
+            // 
+            this.saveMarker.Enabled = false;
+            this.saveMarker.Index = 2;
+            this.saveMarker.Text = "Marker mentése...";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.Multiselect = true;
+            this.openFileDialog1.Title = "Válassza ki a kezelni kívánt hanganyagokat";
             // 
             // MainWindow
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox1);
+            this.Menu = this.mainMenu1;
             this.Name = "MainWindow";
             this.Text = "Main";
+            this.ResizeEnd += new System.EventHandler(this.MainWindow_ResizeEnd);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.MainMenu mainMenu1;
+        private System.Windows.Forms.MenuItem menuItem1;
+        private System.Windows.Forms.MenuItem openAudio;
+        private System.Windows.Forms.MenuItem openMarker;
+        private System.Windows.Forms.MenuItem saveMarker;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
     }
 }
 
--- AudioBookCutter/AudioBookCutter/MainWindow.cs
+++ AudioBookCutter/AudioBookCutter/MainWindow.cs
@@ -7,14 +7,69 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using NAudio.Wave;
+using WaveFormRendererLib;
+using System.IO;
+using System.Drawing.Imaging;
+using NAudio.Wave.SampleProviders;
+using System.Threading;
 
 namespace AudioBookCutter
 {
     public partial class MainWindow : Form
     {
+        private WaveOutEvent output;
+        private AudioFileReader file;
+        private string pathAudio = null;
+        private Image wave;
+
         public MainWindow()
         {
             InitializeComponent();
+            //this.waveViewer1.WaveStream = new WaveFileReader();
+            //pathAudio = @"G:\EKE\szakmai gyak\testaudio.mp3";
+            //audioWave(pathAudio);
+            //pictureBox1.Image = wave;
+        }
+        public void audioWave()
+        {
+            MaxPeakProvider maxPeakProvider = new MaxPeakProvider();
+            RmsPeakProvider rmsPeakProvider = new RmsPeakProvider(200); // e.g. 200
+            SamplingPeakProvider samplingPeakProvider = new SamplingPeakProvider(200); // e.g. 200
+            AveragePeakProvider averagePeakProvider = new AveragePeakProvider(3); // e.g. 4
+
+            StandardWaveFormRendererSettings myRendererSettings = new StandardWaveFormRendererSettings();
+            myRendererSettings.Width = this.Width > 50 ? this.Width - 50 : this.Width;
+            myRendererSettings.TopHeight = 64;
+            myRendererSettings.BottomHeight = 64;
+
+            WaveFormRenderer renderer = new WaveFormRenderer();
+            String audioFilePath = pathAudio;
+            wave = renderer.Render(audioFilePath, averagePeakProvider, myRendererSettings);
+            pictureBox1.Image = wave;
+        }
+
+        private void MainWindow_ResizeEnd(object sender, EventArgs e)
+        {
+            if (pathAudio != null)
+            {
+                pictureBox1.Width = this.Width;
+                Thread t = new Thread(() => audioWave());
+                t.Start();
+                t.Join();
+            }
+        }
+
+        private void openAudio_Click(object sender, EventArgs e)
+        {
+            openFileDialog1 = new OpenFileDialog();
+            openFileDialog1.Filter = "mp3 fájlok|*.mp3|WAV fájlok|*.wav";
+            //openFileDialog1.ShowDialog();
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                pathAudio = openFileDialog1.FileName;
+                audioWave();
+            }
         }
     }
 }
