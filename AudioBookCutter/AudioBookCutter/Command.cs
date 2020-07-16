ceca485ae13aba8d7be9463c567b812c4f58c786
Kornel Mirkovszki <kotn3l@gmail.com>
2020-07-16 14:20:35 +0200

    Command clean up

--- AudioBookCutter/AudioBookCutter/Command.cs
+++ AudioBookCutter/AudioBookCutter/Command.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Diagnostics;
+using System.Threading;
 
 namespace AudioBookCutter
 {
@@ -14,15 +15,16 @@ namespace AudioBookCutter
 
         public Command(string pathToFile)
         {
-            cmd = new Process();
-            ProcessStartInfo startInfo = new ProcessStartInfo();
-            startInfo.WindowStyle = ProcessWindowStyle.Hidden;
-            startInfo.FileName = "cmd.exe";
-            cmd.StartInfo = startInfo;
             this.path = pathToFile;
         }
-        public void cutByTimeSpans(List<TimeSpan> times)
+        public void cutByTimeSpans(List<TimeSpan> times, TimeSpan length)
         {
+            Thread ffmpeg = new Thread(() => cutByTimeSpansIn(times, length));
+            
+        }
+        private void cutByTimeSpansIn(List<TimeSpan> times, TimeSpan length)
+        {
+            init();
             times.OrderBy(time => time.Milliseconds);
             string fileFormat = path.Split('.')[1];
             string argument = "ffmpeg -i " + path;
@@ -34,10 +36,21 @@ namespace AudioBookCutter
             int i = 1;
             while (i < times.Count)
             {
-                temp = argument + " -ss " + times[i-1] + " -to " + times[i] + end + i + fileFormat;
+                temp = argument + " -ss " + times[i - 1] + " -to " + times[i] + end + i + fileFormat;
                 cmd.Start();
+                temp = "";
                 i++;
             }
+            temp = argument + " -ss " + times[i] + " -to "+ length + end + i + fileFormat;
+            cmd.Start();
+        }
+        private void init()
+        {
+            cmd = new Process();
+            ProcessStartInfo startInfo = new ProcessStartInfo();
+            startInfo.WindowStyle = ProcessWindowStyle.Hidden;
+            startInfo.FileName = "cmd.exe";
+            cmd.StartInfo = startInfo;
         }
   
     }
