d8cba40867edeadb595cf8983e4947f2b50769fc
Kornel Mirkovszki <kotn3l@gmail.com>
2020-07-29 14:15:21 +0200

    Saving markers minor fix

--- AudioBookCutter/AudioBookCutter/CUEManager.cs
+++ AudioBookCutter/AudioBookCutter/CUEManager.cs
@@ -16,12 +16,12 @@ namespace AudioBookCutter
             List<TimeSpan> ordered = new List<TimeSpan>(markers.OrderBy(time => time.TotalMilliseconds));
             cue.Title = Path.GetFileNameWithoutExtension(audio.aPath);
             cue.AddTrack("1. fejezet", "na", DataType.AUDIO);
-            cue.AddIndex(0, 1, (int)ordered[0].TotalMinutes, ordered[0].Seconds, ordered[0].Milliseconds/75);
-
-            for (int i = 1; i < ordered.Count; i++)
+            cue.AddIndex(0, 1, 0, 0, 0);
+            int j = 1;
+            for (int i = 0; i < ordered.Count; i++)
             {
-                cue.AddTrack(i + 1 + ". fejezet", "na");
-                cue.AddIndex(i, 1, (int)ordered[i].TotalMinutes, ordered[i].Seconds, ordered[i].Milliseconds / 75);
+                cue.AddTrack(j + 1 + ". fejezet", "na");
+                cue.AddIndex(i + 1, 1, (int)ordered[i].TotalMinutes, ordered[i].Seconds, (int)(ordered[i].Milliseconds / (999 / 75d)));
             }
 
             cue.SaveCue(save);
