4045220a5efb8a8b16c38809620cc1d7b935609f
Kornel Mirkovszki <kotn3l@gmail.com>
2020-07-31 13:03:28 +0200

    Fix total time display on longer files

--- AudioBookCutter/AudioBookCutter/MainWindow.cs
+++ AudioBookCutter/AudioBookCutter/MainWindow.cs
@@ -150,7 +150,7 @@ namespace AudioBookCutter
 
         private static string FormatTimeSpan(TimeSpan ts)
         {
-            return string.Format("{0:D2}:{1:D2}:{2:D2}.{3:D2}", (int)ts.TotalHours, (int)ts.TotalMinutes, ts.Seconds, ts.Milliseconds);
+            return string.Format("{0:D2}:{1:D2}:{2:D2}.{3:D2}", ts.Hours, ts.Minutes, ts.Seconds, ts.Milliseconds);
         }
 
         private void timeLocation()
