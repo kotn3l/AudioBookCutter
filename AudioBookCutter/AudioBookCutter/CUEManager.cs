fb88e78d404124cadad221af0d9d1d707afd9de0
Kornel Mirkovszki <kotn3l@gmail.com>
2020-08-03 15:19:26 +0200

    Modify CUEManager to be able to save/open different kinds of cue

--- AudioBookCutter/AudioBookCutter/CUEManager.cs
+++ AudioBookCutter/AudioBookCutter/CUEManager.cs
@@ -10,7 +10,7 @@ namespace AudioBookCutter
 {
     class CUEManager
     {
-        public void saveMarkers(List<TimeSpan> markers, string save, Audio audio)
+        public void saveMarkersOG(List<TimeSpan> markers, string save, Audio audio)
         {
             CueSheet cue = new CueSheet();
             List<TimeSpan> ordered = new List<TimeSpan>(markers.OrderBy(time => time.TotalMilliseconds));
@@ -28,10 +28,37 @@ namespace AudioBookCutter
             cue.SaveCue(save);
         }
 
+        public void saveMarkersMS(List<TimeSpan> markers, string save, Audio audio)
+        {
+            CueSheet cue = new CueSheet();
+            List<TimeSpan> ordered = new List<TimeSpan>(markers.OrderBy(time => time.TotalMilliseconds));
+            cue.Title = Path.GetFileNameWithoutExtension(audio.aPath);
+            cue.AddTrack("1. fejezet", "na", DataType.AUDIO);
+            cue.AddIndex(0, 1, 0, 0, 0);
+            int j = 1;
+            for (int i = 0; i < ordered.Count; i++)
+            {
+                j++;
+                cue.AddTrack(j + ". fejezet", "na");
+                cue.AddIndex(i + 1, 1, (int)ordered[i].TotalMinutes, ordered[i].Seconds, ordered[i].Milliseconds);
+            }
+
+            cue.SaveCue(save);
+        }
+
         public List<Marker> openMarkers(string path)
         {
             CueSheet cue = new CueSheet(path);
             List<Marker> markers = new List<Marker>();
+            bool ms = false;
+            for (int i = 0; i < cue.Tracks.Length; i++)
+            {
+                if (cue[i][0].Frames.ToString().Length == 3)
+                {
+                    ms = true;
+                    break;
+                }
+            }
             for (int i = 1; i < cue.Tracks.Length; i++)
             {
                 TimeSpan fromMins = TimeSpan.FromMinutes(cue[i][0].Minutes);
@@ -39,7 +66,7 @@ namespace AudioBookCutter
                     0, (int)fromMins.TotalHours,
                     fromMins.Minutes,
                     cue[i][0].Seconds,
-                    (int)(cue[i][0].Frames * (1000/75d)))));
+                    ms ? cue[i][0].Frames : (int)(cue[i][0].Frames * (1000/75d) ))));
             }
             return markers;
         }
