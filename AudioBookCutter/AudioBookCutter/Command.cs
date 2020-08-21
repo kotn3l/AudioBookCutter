605a1090145d25d4db1064aafc2d9bd53956ab57
Kornel Mirkovszki <kotn3l@gmail.com>
2020-08-21 15:20:53 +0200

    FFmpeg cut argument change

--- AudioBookCutter/AudioBookCutter/Command.cs
+++ AudioBookCutter/AudioBookCutter/Command.cs
@@ -10,7 +10,7 @@ namespace AudioBookCutter
     class Command
     {
         ProcessStartInfo startInfo;
-        private string argumentStart = "-i ";
+        private string argumentStart = " -i ";
         private string workingDir = Path.GetDirectoryName(System.Reflection.Assembly.GetEntryAssembly().Location);
         private string temp = @"\temp\";
         private string command = "[COMMAND] ";
@@ -32,20 +32,20 @@ namespace AudioBookCutter
             string argument = argumentStart + "\"" + Path.GetFullPath(path) + "\"";
             string end = " -c copy " + "\"" + save;
             string temp;
-            temp = argument + " -ss 00:00:00.0 -to " + ordered[0] + end + 1.ToString().PadLeft(3, '0') + fileFormat + "\"";
+            temp = "-ss 00:00:00.0 -to " + ordered[0] + argument + end + 1.ToString().PadLeft(3, '0') + fileFormat + "\"";
             Execute(temp);
             Log.Information(command + "Cut from 00:00:00.0 to {0} done", ordered[0]);
             temp = "";
             int i = 1;
             while (i < ordered.Count)
             {
-                temp = argument + " -ss " + ordered[i - 1] + " -to " + ordered[i] + end + (i+1).ToString().PadLeft(3, '0') + fileFormat + "\"";
+                temp = "-ss " + ordered[i - 1] + " -to " + ordered[i] + argument + end + (i+1).ToString().PadLeft(3, '0') + fileFormat + "\"";
                 Execute(temp);
                 Log.Information(command + "Cut from {0} to {1} done", ordered[i - 1], ordered[i]);
                 temp = "";
                 i++;
             }
-            temp = argument + " -ss " + ordered[i-1] + " -to "+ length + end + (i+1).ToString().PadLeft(3, '0') + fileFormat + "\"";
+            temp = "-ss " + ordered[i-1] + " -to "+ length + argument + end + (i+1).ToString().PadLeft(3, '0') + fileFormat + "\"";
             Execute(temp);
             Log.Information(command + "Cut from {0} to {1} done", ordered[i - 1], length);
             Log.Information(command + "{0} files were saved", i+1);
